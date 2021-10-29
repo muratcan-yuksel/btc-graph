27951eef92a1930667d216c900145ffe8078f393
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-29 21:55:32 +0300

    beginning of the project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>HomePage</h1>
+  <Btc />
+</template>
+
+<script>
+import Btc from "./components/Btc.vue";
+
+export default {
+  components: {
+    Btc,
+  },
+};
+</script>
+
+<style></style>
